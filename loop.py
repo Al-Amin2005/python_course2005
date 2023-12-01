b2c6cbb61366a1d07a9e5ad057519f22477d5373
Al-Amin2005 <islambadhon219@gmail.com>
2023-12-02 01:11:50 +0600

    comment

--- loop.py
+++ loop.py
@@ -1,8 +1,22 @@
-name ="alamin"
+#name ="alamin"
 # print(name[0])
+#sum = 0
+#for i in range(7):
+#    print(i)#0,97,108,97,109,105,110
+#    sum = sum + i #0 #97 #205 #302 #411 #516 #626
+    
+#print(sum)
+
+name = "alamin"
+
+#print(name[0])
+
+#for i in range(len(name)):
+ #   print(name[i])
+
 sum = 0
 for i in range(7):
-    print(i)#0,97,108,97,109,105,110
-    sum = sum + i #0 #97 #205 #302 #411 #516 #626
-    
-print(sum)
+    print(i)
+    sum = sum + i
+
+    print(sum)
---
+++ string.py
@@ -0,0 +1,9 @@
+name = "Alamin"
+size = len(name)
+check_upper = name[0].isupper()
+if (check_upper):
+    print("the first letter capital")
+else:
+    print("the first letter is not capital")
+
+ print(name.capitalize())
