baa674747db3fa09cab1dc7563bc0d7d8b12aa65
Al-Amin2005 <islambadhon219@gmail.com>
2023-12-07 00:16:32 +0600

    dictionary files added

---
+++ dictionary.py
@@ -0,0 +1,7 @@
+cars ={"car1":
+    {"model":"m1","color":"blue","price":"100"},
+    "car2":
+     {"mode2":"m","color":"red","price":"1000"}}
+for i,j in cars. items():
+    for k in j. items():
+        print(i,k)
